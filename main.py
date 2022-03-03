ea987b07f55bae6d6be424f75d20175b3e516487
jxck <85757671+jxckss@users.noreply.github.com>
2022-03-03 20:49:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-﻿# Name
+#
 name = input("What is your name? ")
 
 # 
 age = input("What is your age in years? ")
 age = int(age)
 hourage = (age * 365 * 24)
+mins = (hourage * 60)
+seconds = (mins * 60)
 
-# Output
+#
 print(name, "has been alive for:", age * 365, "Days")
 print("Or:", hourage, "Hours")
 print("Or:", hourage * 60, "Minutes")
 print("Or:", hourage * 60 * 60, "Seconds")
+print("You are:", age, "Years Old, Or", hourage, "Hours Old, Or", mins, "Minutes Old, Or", seconds, "Seconds Old!")
