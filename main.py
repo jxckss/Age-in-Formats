186dc69f780317a3ced4b58da455c3afb493d3dd
jxck <85757671+jxckss@users.noreply.github.com>
2022-03-03 20:43:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿# Name
+name = input("What is your name? ")
+
+# 
+age = input("What is your age in years? ")
+age = int(age)
+hourage = (age * 365 * 24)
+
+# Output
+print(name, "has been alive for:", age * 365, "Days")
+print("Or:", hourage, "Hours")
+print("Or:", hourage * 60, "Minutes")
+print("Or:", hourage * 60 * 60, "Seconds")
